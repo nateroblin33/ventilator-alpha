2f22641ed884219627d947cdabbe3e112c2f936c
Kyle Bannerman <bannermanky@gmail.com>
2021-02-22 14:52:53 -0500

    add. include button interface

---
+++ main/Buttons.h
@@ -0,0 +1,14 @@
+/* 
+ * Filename: Buttons.h
+ * Author: Kyle Bannerman
+ * Date Created: 02/22/2021
+ * Date of Last Edit: 02/22/2021
+ * File Purpose: interfacing with buttons
+ */
+
+
+// retrieve the state of the button from digital read
+bool read_button(int pin);
+
+
+
