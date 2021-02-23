ae1b7dbd8e5ec8c65e2fc7bf3c9728412d5ebd43
Kyle Bannerman <bannermanky@gmail.com>
2021-02-23 11:10:19 -0500

    add. include LCD screen interface

---
+++ main/LCDScreen.h
@@ -0,0 +1,22 @@
+/* 
+ * Filename: LCDScreen.h
+ * Author: Kyle Bannerman
+ * Date Created: 02/23/2021
+ * Date of Last Edit: 02/23/2021
+ * File Purpose: interfacing with LCD screen using I2C
+ */
+
+
+// init screen
+void init_screen();
+
+// write to screen through serial
+void write_screen(int col, int row, char *msg);
+
+
+// clear the screen
+void clear_screen();
+
+
+// write the pressure values to the LCD screen
+void write_pressure_vals(float input_pressure, float output_pressure);
