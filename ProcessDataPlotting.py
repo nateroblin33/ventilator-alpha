0855f6403a194a2400fecc8ceb4b555d048f4962
Kyle Bannerman <bannermanky@gmail.com>
2021-03-25 17:56:15 -0400

    minor. fix dependency relation for matplotlib

--- ProcessDataPlotting.py
+++ ProcessDataPlotting.py
@@ -11,6 +11,10 @@ import pandas as pd
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+
+import matplotlib
+matplotlib.use("TkAgg")
+
 import matplotlib.pyplot as plt
 
 
