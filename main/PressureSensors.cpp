15a03cca80fe51bb79627aeaa28615d6f43cc8d5
Kyle Bannerman <bannermanky@gmail.com>
2021-04-09 14:37:16 -0400

    fix. correct voltage conversion for our sensor

--- main/PressureSensors.cpp
+++ main/PressureSensors.cpp
@@ -2,7 +2,7 @@
  * Filename: PressureSensors.cpp
  * Author: Kyle Bannerman
  * Date Created: 02/22/2021
- * Date of Last Edit: 02/22/2021
+ * Date of Last Edit: 04/09/2021
  * File Purpose: interfacing with pressure sensors
  */
 
@@ -13,13 +13,10 @@
 // convert voltage to pressure value
 float convert_pressure(int v)
 {
-  float offset = 200.0;
-  float sensitivity = 4.413;
-  float adc_mv = 4.8828125;
-
-  float mmh20_kpa = 0.00981;
+  float offset = 2.6103;
+  float slope = 0.0827;
   
-  return (v * adc_mv - offset) / sensitivity * mmh20_kpa;
+  return v * adc_mv - offset;
 }
 
 
