8606e0df10f20a15acdee86dadafefdbc5fcd5bf
Kyle Bannerman <bannermanky@gmail.com>
2021-04-12 18:21:34 -0400

    add. include step speed control based on desired flow rate

--- main/main.ino
+++ main/main.ino
@@ -1,71 +1,71 @@
 /* 
  * Filename: main.ino
  * Author: Kyle Bannerman
- * Date Created: 02/19/2021
- * Date of Last Edit: 02/23/2021
+ * Date Created: 04/12/2021
+ * Date of Last Edit: 04/12/2021
  * File Purpose: main controller of arduino
  */
+ 
+// include the Arduino Stepper Library
+#include <Stepper.h>
 
+// number of steps per output rotation
+const int STEPS_PER_REV = 200;
 
-#include "_init.h"
-#include "PressureSensors.h"
-#include "Buttons.h"
-#include "LEDs.h"
-#include "LCDScreen.h"
+// create instance of stepper library
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
+// maximum allowed instantaneous flow rate
+const float MAX_INST_FLOW = 2.0;
 
-// user defined constants
-#define PRESSURE_MAX 100
-#define PRESSURE_MIN 0
+// array of flow rates 
+const int FLOWRATES_SIZE = 10;
+const float flowRates[FLOWRATES_SIZE] = { 8.0, 7.0, 6.75, 6.5, 6.25, 6.0, 5.75, 5.5, 5.25, 5.0 }
 
-
-// init main
-void setup() 
+// calculate speed necessary to produce desired flow rate -- based on experimental values
+float calcSpeed(float flowRate)
 {
-  
-  // init pressure alert RGB LED pins
-  pinMode(PRESS_R_PIN, OUTPUT);
-  pinMode(PRESS_G_PIN, OUTPUT);
-  pinMode(PRESS_B_PIN, OUTPUT);
+  return 34.8423 * flowRate - 94.9821;
+}
 
-  // init battery alert RGB LED pins
-  pinMode(BATT_R_PIN, OUTPUT);
-  pinMode(BATT_G_PIN, OUTPUT);
-  pinMode(BATT_B_PIN, OUTPUT);
+// calculate instantaneous flow rate from speed -- based on experimental values
+float calcInstFlowRate(float speedEst)
+{
+  return 0.0165 * speedEst - 0.617;
+}
 
-  // init default setting button pins
-  pinMode(DEF0_BUT_PIN, INPUT);
-  pinMode(DEF1_BUT_PIN, INPUT);
-  pinMode(DEF2_BUT_PIN, INPUT);
-
-  // init LCD screen
-  init_screen();
+// check if the instantaneous flow rate exceeds the maximum allowed -- true if safe, false otherwise
+bool checkSpeed(float speedEst)
+{
+  return calcInstFlowRate(speedEst) <= MAX_INST_FLOW;
 }
 
 
+void setup()
+{
+  Serial.begin(9600);
+}
+
 void loop() 
 {
+  float speedEst;
+  for (int i = 0; i < FLOWRATES_SIZE; i++)
+  {
+    speedEst = calcSpeed(flowRates[i]);
     
-  // read in pressure sensor values
-  float input_pressure = read_pressure(PRESS_I_PIN);
-  float output_pressure = read_pressure(PRESS_O_PIN);
-  
-  // adjust pressure warning LED depending on pressure level
-  if (input_pressure > PRESSURE_MAX || input_pressure < PRESSURE_MIN) 
-    write_RGB(PRESS_R_PIN, PRESS_G_PIN, PRESS_B_PIN, 255, 0, 0); // red if an issue
-  else
-    write_RGB(PRESS_R_PIN, PRESS_G_PIN, PRESS_B_PIN, 0, 255, 0); // green if no issues
-
-  // write to battery status LEDs
-  write_RGB(BATT_R_PIN, BATT_G_PIN, BATT_B_PIN, 0, 255, 0);
-
-  // check for default setting button pressure
-  int default_selected = -1;
-  if (read_button(DEF0_BUT_PIN)) default_selected = 0;
-  else if (read_button(DEF1_BUT_PIN)) default_selected = 1;
-  else if (read_button(DEF2_BUT_PIN)) default_selected = 2;
-
-  // write to LCD screen
-  clear_screen();
-  write_pressure_vals(input_pressure, output_pressure);
+    myStepper.setSpeed(speedEst);
+    
+    for (int j = 0; j < 5; j++)
+    {
+      myStepper.step(STEPS_PER_REV);
+    
+      delay(500);
+    
+      myStepper.step(-STEPS_PER_REV);
+      
+      delay(500);
+    }
+    
+    delay(5000);
+  }
 }
