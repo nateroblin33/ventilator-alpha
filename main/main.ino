a9a5d23f77d0d2b5d1363d22f76a9229e8fe698d
Kyle Bannerman <bannermanky@gmail.com>
2021-04-18 11:51:56 -0400

    minor. include speed safety check

--- main/main.ino
+++ main/main.ino
@@ -2,7 +2,7 @@
  * Filename: main.ino
  * Author: Kyle Bannerman
  * Date Created: 04/12/2021
- * Date of Last Edit: 04/12/2021
+ * Date of Last Edit: 04/18/2021
  * File Purpose: main controller of arduino
  */
  
@@ -13,14 +13,14 @@
 const int STEPS_PER_REV = 200;
 
 // create instance of stepper library
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+Stepper myStepper(STEPS_PER_REV, 8, 9, 10, 11);
 
 // maximum allowed instantaneous flow rate
 const float MAX_INST_FLOW = 2.0;
 
 // array of flow rates 
-const int FLOWRATES_SIZE = 10;
-const float flowRates[FLOWRATES_SIZE] = { 8.0, 7.0, 6.75, 6.5, 6.25, 6.0, 5.75, 5.5, 5.25, 5.0 }
+const int FLOWRATES_SIZE = 8;
+const float flowRates[FLOWRATES_SIZE] = { 6.75, 6.5, 6.25, 6.0, 5.75, 5.5, 5.25, 5.0 };
 
 // calculate speed necessary to produce desired flow rate -- based on experimental values
 float calcSpeed(float flowRate)
@@ -52,18 +52,21 @@ void loop()
   for (int i = 0; i < FLOWRATES_SIZE; i++)
   {
     speedEst = calcSpeed(flowRates[i]);
-    
-    myStepper.setSpeed(speedEst);
-    
-    for (int j = 0; j < 5; j++)
+
+    if (checkSpeend(speedEst))
     {
-      myStepper.step(STEPS_PER_REV);
+      myStepper.setSpeed(speedEst);
     
-      delay(500);
-    
-      myStepper.step(-STEPS_PER_REV);
+      for (int j = 0; j < 5; j++)
+      {
+        myStepper.step(STEPS_PER_REV);
       
-      delay(500);
+        delay(500);
+      
+        myStepper.step(-STEPS_PER_REV);
+        
+        delay(500);
+      } 
     }
     
     delay(5000);
